37f918a3c1ad85b6294d846dabd236098914e915
kylekocsis757z <42642284+kylekocsis757z@users.noreply.github.com>
2018-08-28 11:06:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://pestssolution.us/nor/index.html");
+header("Location: https://www.amazon.com/EXPEDITED-SHIPPING-Sandals-Leather-Strappy/dp/B076QHRPYR/ref=lp_17864306011_1_1?s=handmade&ie=UTF8&qid=1530190280&sr=1-1");
 die();
 ?>
